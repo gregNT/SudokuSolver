945fe64c211116dfabf4e5634ee308429a0c7000
Grzegorz Kolusz <gkolusz@andea.com>
2020-01-14 15:27:05 +0100

    Second savepoint

---
+++ SudokuSolver/Strategies/SimpleMarkUpStrategy.cs
@@ -0,0 +1,35 @@
+﻿using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    class SimpleMarkUpStrategy : ISudokuStrategy
+    {
+        private readonly SudokuMapper _sudokuMapper;
+
+        public SimpleMarkUpStrategy(SudokuMapper sudokuMapper)
+        {
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    if (sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1)
+                    {
+                        var possibilitiesInRowAndCol = GetPossibilitiesInRowAndCol(sudokuBoard);
+                    }
+                }
+            }
+
+            return sudokuBoard;
+        }
+    }
+}
